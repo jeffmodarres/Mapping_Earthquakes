c163ab55ec44772e52bb54892ce36a0bf7e11006
jeffmodarres <99626528+jeffmodarres@users.noreply.github.com>
2022-05-19 23:25:25 -0400

    end of 13.5.5

--- Mapping_GeoJSON_Linestrings/static/js/logic1.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic1.js
@@ -1,44 +1,34 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
+// An array containing each city's location, state, and population.
+let citiesData = cities
 
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-let baseMaps = {
-    Street: streets,
-    Dark: dark
-  };
-
-// Create the map object with center and zoom level.
-let map = L.map('mapid', {
-    center: [30, 30],
-    zoom: 4,
-    layers: [streets]
-})
 // Add GeoJSON data.
-
-d3.json('./majorAirports.json').then(function(data) {
-// console.log(data);
-L.geoJSON(data , {
-onEachFeature: function(feature, layer) {
-          console.log(feature);
-          return layer.bindPopup("<h2>" + feature.properties.city + "</h2> <hr> <h4>" +feature.properties.name + "</h4> ");
-}}).addTo(map);
-})
-
-
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
 // Grabbing our GeoJSON data.
-// L.geoJSON(sanFranAirport).addTo(map);
+L.geoJSON(sanFranAirport).addTo(map);
 
 // / Grabbing our GeoJSON data.
 // L.geoJSON(sanFranAirport, {
@@ -50,14 +40,14 @@ onEachFeature: function(feature, layer) {
 
 //   }).addTo(map);
 
-// L.geoJSON(sanFranAirport, {
-//     // We turn each feature into a marker on the map.
-//     onEachFeature: function(feature, layer) {
-//       console.log(feature);
-//       return layer.bindPopup("<h2>" + feature.properties.city + "</h2> <hr> <h4>" +feature.properties.name + "</h4> ");
-//     }
+L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    onEachFeature: function(feature, layer) {
+      console.log(feature);
+      return layer.bindPopup("<h2>" + feature.properties.city + "</h2> <hr> <h4>" +feature.properties.name + "</h4> ");
+    }
 
-//   }).addTo(map);
+  }).addTo(map);
 
 // citiesData.forEach( function(city) {
 //     console.log(city)
@@ -94,9 +84,11 @@ onEachFeature: function(feature, layer) {
 //  }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-
-
-  L.control.layers(baseMaps).addTo(map);
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+}).addTo(map);
 
 // Then we add our 'graymap' tile layer to the map.
 // streets.addTo(map);
