8ca7927fe282f507de53f4e73345a4afe30a7019
jeffmodarres <99626528+jeffmodarres@users.noreply.github.com>
2022-05-19 23:16:28 -0400

    end of 13.5.5

--- Mapping_GeoJSON_Points/static/js/logic1.js
+++ Mapping_GeoJSON_Points/static/js/logic1.js
@@ -30,6 +30,25 @@ let sanFranAirport =
 // Grabbing our GeoJSON data.
 L.geoJSON(sanFranAirport).addTo(map);
 
+// / Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport, {
+//     // We turn each feature into a marker on the map.
+//     pointToLayer: function(feature, latlng) {
+//       console.log(feature);
+//       return L.marker(latlng).bindPopup("<h2>" + feature.properties.city + "</h2>");
+//     }
+
+//   }).addTo(map);
+
+L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    onEachFeature: function(feature, layer) {
+      console.log(feature);
+      return layer.bindPopup("<h2>" + feature.properties.city + "</h2> <hr> <h4>" +feature.properties.name + "</h4> ");
+    }
+
+  }).addTo(map);
+
 // citiesData.forEach( function(city) {
 //     console.log(city)
 
@@ -65,11 +84,11 @@ L.geoJSON(sanFranAirport).addTo(map);
 //  }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     accessToken: API_KEY
-// });
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+}).addTo(map);
 
 // Then we add our 'graymap' tile layer to the map.
 // streets.addTo(map);
